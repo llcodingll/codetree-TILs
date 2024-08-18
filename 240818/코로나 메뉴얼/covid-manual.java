9bb662c351959d16709d53a267149a0c7399600b
유윤지 <105447233+llcodingll@users.noreply.github.com>
2024-08-18 15:10:33 +0900

    Title: 코로나 메뉴얼, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240818/코로나 메뉴얼/covid-manual.java
+++ 240818/코로나 메뉴얼/covid-manual.java
@@ -4,24 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int cnt = 0;
-        for(cnt = 0; cnt <= 3; cnt++){
-            char a = sc.next().charAt(0);
-            int b = sc.nextInt();
-        
+        int cnt = 0;  // A로 분류된 사람 수를 세기 위한 변수
 
-        if(a=='Y' && b>=37){
-            cnt++;
-        } else if(a=='N' && b>=37){
+        // 3명의 입력을 받기 위해 반복
+        for(int i = 0; i < 3; i++){
+            char sym = sc.next().charAt(0);  // 감기 증상 (Y/N)
+            int temp = sc.nextInt();      // 체온 (0 ≤ 주어지는 체온 ≤ 40)
 
-        } else if(a=='Y' && b<37){
-
-        } else {
-
-        }
+            // 조건에 따라 분류
+            if(sym == 'Y' && temp >= 37){
+                cnt++;  // A로 분류된 사람 수 증가
+            }
         }
         
-        if(cnt>=2){
+        // A로 분류된 사람이 2명 이상이면 위급상황
+        if(cnt >= 2){
            System.out.print("E");
         } else{
             System.out.print("N");
